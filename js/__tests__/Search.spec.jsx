27e4c218462995bf0ea87b773b751776f32934ed
Carlton Caven <carlcaven@gmail.com>
2018-08-20 12:11:57 -0700

    add tests for search

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import renderer from "react-test-renderer";
+// import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+// enzyme uses renderer underneath the hood
+import preload from "../..data.json";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
 test("Search renders correctly", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  // const tree = component.toJSON();
+  expect(component).toMatchSnapshot();
 });
 
-test("Search renders correctly", () => {
-  const component = renderer.create(<ShowCard />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+test("Search should render correct amount of shows", () => {
+  const component = shallow(<Search />);
+  expect(preload.shows.length).toEqual(component.find(ShowCard).length);
 });
 
-test("Search renders correctly", () => {
-  const component = renderer.create(<Landing />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+test("Search should render correct amount of shows based on search term", () => {});
