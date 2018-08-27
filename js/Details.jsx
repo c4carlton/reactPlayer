626289201d2a04837f874e720b6734965afb3ef7
Carlton Caven <carlcaven@gmail.com>
2018-08-26 20:40:04 -0700

    update details.jsx

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,9 +2,9 @@
 
 import React from "react";
 
-const Details = () => (
+const Details = props => (
   <div className="details">
-    <h1>Hi Lol</h1>
+    <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
   </div>
 );
 
